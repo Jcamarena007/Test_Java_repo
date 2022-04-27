fe5fc1b857881c3905c3046e1e4434099606240b
Jcamarena007 <104379568+Jcamarena007@users.noreply.github.com>
2022-04-27 10:24:45 -0500

    Update SumOfInput.java

--- SumOfInput.java
+++ SumOfInput.java
@@ -1,20 +1,10 @@
-/**
- * Write a description of class SumOfInput here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
+
 public class SumOfInput
 {
-    // instance variables - replace the example below with your own
     private Integer sum;
 
-    /**
-     * Constructor for objects of class SumOfInput
-     */
     public SumOfInput()
     {
-        // initialise instance variables
         sum = 0;
     }
     
